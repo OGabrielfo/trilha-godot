a3dc5f036d81feca0e359b633dcc431ac9eeb4fc
OGabrielfo <ogabrielfo.silva@gmail.com>
2024-07-27 18:18:21 -0300

    Camera e movimento
    Melhorias na câmera e no movimento do player para envio do desafio.

--- Tiny Swords - Parte 4, Desafio/player/player.gd
+++ Tiny Swords - Parte 4, Desafio/player/player.gd
@@ -71,7 +71,7 @@ func _physics_process(delta: float) -> void:
 	var target_velocity = input_vector * speed * 100.0
 	if is_attacking:
 		target_velocity *= 0.25
-	velocity = lerp(velocity, target_velocity, 0.05)
+	velocity = lerp(velocity, target_velocity, 0.1)
 	move_and_slide()
 
 
